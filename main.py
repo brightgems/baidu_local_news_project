bdc11f373b27314a6b5ef29b0372d793684020c7
pjyuan <ypj1992@126.com>
2017-06-22 20:32:32 +0800

    fix news_profile path encoding

--- main.py
+++ main.py
@@ -27,10 +27,11 @@ city_remender_tuple_list = conf.ReminderConf.city_remender_tuple_list
 reminde_time = conf.ReminderConf.reminde_time
 
 while True:
-    utils.timer(conf.SpiderConf.SPIDER_HOUR_RUN_TIME)
+    # utils.timer(conf.SpiderConf.SPIDER_HOUR_RUN_TIME)
 
     nowtime = datetime.datetime.now()
-    if nowtime.hour == reminde_time: ###如果是7点，则进行邮件推送
+    # if nowtime.hour == reminde_time: ###如果是7点，则进行邮件推送
+    if nowtime.hour == 20: ###如果是7点，则进行邮件推送
         yesterday_date_string = utils.get_yesterday_date_string()
         # yesterday_date_string = '2017-06-11'
 
@@ -38,7 +39,7 @@ while True:
         sorted_city_newscount, sorted_city_risklevel = city_risklevel_profile.daily_risklevel_profile(yesterday_date_string, city_tuple_list, place_news_base_path = news_basepath, risk_level_basepath = risklevel_profile_basepath, path_encoding = encoding)
 
         #step2: 计算每日的新闻画像，并保存到本地
-        news_profile.daily_news_profile(city_tuple_list, news_basepath, news_profile_basepath, yesterday_date_string, reserved_word_path)
+        news_profile.daily_news_profile(city_tuple_list, news_basepath, news_profile_basepath, yesterday_date_string, reserved_word_path, path_encoding=encoding)
 
         #step3: 计算每日城市风险画像
         city_profile_list = city_profile.daily_city_profile(city_tuple_list, yesterday_date_string, news_profile_basepath, city_profile_basepath, path_encoding = encoding )
